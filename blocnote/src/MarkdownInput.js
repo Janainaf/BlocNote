87e5fb9da19f34be513a80a56a9fe194e8dd666a
Janainaf <figueira.janaina@gmail.com>
2021-05-06 10:42:30 +0200

    added markdown

--- blocnote/src/MarkdownInput.js
+++ blocnote/src/MarkdownInput.js
@@ -1,8 +1,12 @@
+import ReactMarkdown from "react-markdown";
+
 function MarkdownInput({ activeNote }) {
   return (
     <div className="app-main-note-preview">
       <h1 className="preview-title"> {activeNote.title}</h1>
-      <div className="markdown-preview"> {activeNote.body}</div>
+      <ReactMarkdown className="markdown-preview">
+        {activeNote.body}
+      </ReactMarkdown>
     </div>
   );
 }
