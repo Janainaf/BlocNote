6472a73705b4815e204ec284b90af0e237bd52b9
Janainaf <figueira.janaina@gmail.com>
2021-05-06 10:35:39 +0200

    fixed preview

--- blocnote/src/MarkdownInput.js
+++ blocnote/src/MarkdownInput.js
@@ -2,7 +2,7 @@ function MarkdownInput({ activeNote }) {
   return (
     <div className="app-main-note-preview">
       <h1 className="preview-title"> {activeNote.title}</h1>
-      <div className="markdown-preview"> note preview</div>
+      <div className="markdown-preview"> {activeNote.body}</div>
     </div>
   );
 }
