5b556407050ca673b12294a5f4da33a8f1cbfc7b
Janainaf <figueira.janaina@gmail.com>
2021-05-06 21:01:33 +0200

    added showdown but prefer to use markdown library

--- blocnote/src/MarkdownInput.js
+++ blocnote/src/MarkdownInput.js
@@ -1,9 +1,14 @@
 import ReactMarkdown from "react-markdown";
+import Showdown from "showdown";
+const converter = new Showdown.Converter();
 
 function MarkdownInput({ activeNote }) {
   return (
     <div className="app-main-note-preview">
       <h1 className="preview-title"> {activeNote.title}</h1>
+      {/* <div className="markdown-preview">
+        {converter.makeHtml(activeNote.body)}
+      </div> */}
       <ReactMarkdown className="markdown-preview">
         {activeNote.body}
       </ReactMarkdown>
